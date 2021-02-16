26bc10745e23a72a37fa2888b8919c14d17f3712
Kingsley Charles <kingsley.charles@global.com>
2021-02-16 10:34:55 +0000

    EP 1 Done

---
+++ Podcast/MainTabBarController.swift
@@ -0,0 +1,36 @@
+//
+//  MainTabBarController.swift
+//  Podcast
+//
+//  Created by Kingsley Charles on 16/02/2021.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUpTabBar()
+        
+    }
+    
+    func setUpTabBar() {
+        self.tabBar.tintColor = .purple
+        viewControllers = [setUpNavControllers(viewController: ViewController(), image: #imageLiteral(resourceName: "favorites"), title: "Favourites"),
+                           setUpNavControllers(viewController: ViewController(), image: #imageLiteral(resourceName: "search"), title: "Search"),
+                           setUpNavControllers(viewController: ViewController(), image: #imageLiteral(resourceName: "downloads"), title: "Downloads"),]
+    }
+    
+    fileprivate func setUpNavControllers(viewController: UIViewController, image: UIImage, title: String) -> UINavigationController {
+        let vc = viewController
+        let templateNavController = UINavigationController(rootViewController: vc)
+        templateNavController.tabBarItem.image = image
+        templateNavController.tabBarItem.title = title
+        templateNavController.navigationBar.prefersLargeTitles = true
+        templateNavController.topViewController?.title = title
+        return templateNavController
+    }
+    
+    
+}
---
+++ Podcast/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Podcast
+//
+//  Created by Kingsley Charles on 16/02/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+    }
+
+
+}
+
